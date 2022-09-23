9316c4ee25a57b9099d1478c53c392095e4c6d07
Jheison Rua <ruateo555@gmail.com>
2022-09-23 14:37:44 -0500

    segunda parte

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import '../cssComponents/Main.css'
+import pc from '../images/pc.jpeg'
 const Main = ()=>{
     return (
         <div className='container-main'>
             <div className='image-main'>
-
+                <img src={pc} alt='pc'/>
             </div>
-            <div></div>
+            <div className='caja-textos'>
+                <h2>Expertos en Desarrollo Web</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio doloremque enim ad, quo mollitia hic illo dolores explicabo officia voluptas recusandae sint qui saepe consequatur numquam unde error eaque ab.</p>
+            
+            </div>
             <div></div>
         </div>
     )
